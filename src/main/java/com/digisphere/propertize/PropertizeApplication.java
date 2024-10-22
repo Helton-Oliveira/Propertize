1531c75314dfcf358dcd9aef67ea2a43d66e93cc
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-10-22 12:06:59 -0300

    config: add files

---
+++ src/main/java/com/digisphere/propertize/PropertizeApplication.java
@@ -0,0 +1,13 @@
+package com.digisphere.propertize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digisphere/propertize/PropertizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digisphere.propertize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
