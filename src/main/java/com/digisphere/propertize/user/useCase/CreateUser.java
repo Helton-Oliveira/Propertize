d7291054c44af6fd9ac9f0caca8d35267d3366fd
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-10-23 11:57:55 -0300

    refactor: refactoring user creation method

--- src/main/java/com/digisphere/propertize/user/useCase/CreateUser.java
+++ src/main/java/com/digisphere/propertize/user/useCase/CreateUser.java
@@ -1,16 +1,22 @@
 package com.digisphere.propertize.user.useCase;
 
-import com.digisphere.propertize.user.builderPattern.director.Director;
 import com.digisphere.propertize.user.domain.User;
+import com.digisphere.propertize.user.useCase.strategyPattern.context.IContext;
 
 import java.util.Map;
 
 public class CreateUser implements ICreateUser {
 
+    private final IContext context;
+
+    public CreateUser(IContext context) {
+        this.context = context;
+    }
+
     @Override
     public User execute(Map<String, String> attributes) {
-        var user = Director.createDirector();
-        user.createUser(attributes.get("name"), attributes.get("email"), attributes.get("cpf"), attributes.get("password"), attributes.get("phone"), attributes.get("role"));
-        return user.build();
+        context.setStrategy(attributes);
+
+        return context.executeStrategy(attributes);
     }
 }
