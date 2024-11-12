ff6d2b8902508b8eb3094a623a8a73b69ccbe6b4
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-11-12 16:06:03 -0300

    feat: implementing API for data entry into the system

--- src/main/java/com/digisphere/propertize/adapter/controller/UserController.java
+++ src/main/java/com/digisphere/propertize/adapter/controller/UserController.java
@@ -1,9 +1,72 @@
-/*
 package com.digisphere.propertize.adapter.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.digisphere.propertize.adapter.adapterPattern.userAdapter.IAdapterUser;
+import com.digisphere.propertize.adapter.dtos.user.CreateUserDto;
+import com.digisphere.propertize.adapter.dtos.user.OutputUser;
+import com.digisphere.propertize.adapter.dtos.user.UserReferenceDto;
+import com.digisphere.propertize.application.user.useCase.interfaces.IDisableUser;
+import com.digisphere.propertize.application.user.useCase.interfaces.IGetAllUsers;
+import com.digisphere.propertize.application.user.useCase.interfaces.IGetOneUSer;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final IAdapterUser adapter;
+    private final IGetAllUsers getAllUsers;
+
+    public UserController(IAdapterUser adapter, IGetAllUsers getAllUsers, IGetOneUSer getOneUSer, IDisableUser disableUser) {
+        this.adapter = adapter;
+        this.getAllUsers = getAllUsers;
+
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<?> createUser(@RequestBody @Valid CreateUserDto dto, UriComponentsBuilder uriBuilder) {
+      var user = adapter.adaptCreateUserRequest(dto);
+
+      var uri = uriBuilder.path("/users/{cpf}").buildAndExpand(user.cpf()).toUri();
+      return ResponseEntity.created(uri).body(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OutputUser>> getAll() {
+        List<OutputUser> outputUsers = getAllUsers.execute().stream()
+                .map(u -> {
+                    return new OutputUser(u.getName(), u.getEmail(), u.getCpf(), u.getPhone(), u.getRole().toString());
+                })
+                .toList();
+
+        return ResponseEntity.ok().body(outputUsers);
+    }
+
+    @GetMapping("/one")
+    public ResponseEntity<OutputUser> getOne(@RequestBody @Valid UserReferenceDto dto) {
+        OutputUser outputUser = adapter.adaptFindUserRequest(dto);
+
+        return ResponseEntity.ok(outputUser);
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity<String> updateUser(@RequestBody @Valid UserReferenceDto dto) {
+        String response = adapter.adaptUpdateUserRequest(dto);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity<String> disableUser(@RequestBody @Valid UserReferenceDto dto) {
+        String response = adapter.adaptDeactivateUserRequest(dto);
+        return ResponseEntity.ok(response);
+    }
 }
-*/
---
+++ src/test/java/com/digisphere/propertize/IntegrationTests/UserAdapterTest.java
@@ -0,0 +1,79 @@
+package com.digisphere.propertize.IntegrationTests;
+
+import com.digisphere.propertize.adapter.adapterPattern.userAdapter.IAdapterUser;
+import com.digisphere.propertize.adapter.adapterPattern.userAdapter.UserAdapter;
+import com.digisphere.propertize.adapter.dtos.user.CreateUserDto;
+import com.digisphere.propertize.adapter.dtos.user.OutputUser;
+import com.digisphere.propertize.adapter.dtos.user.UserReferenceDto;
+import com.digisphere.propertize.application.director.TemplateMethodPattern.TemplateClass.ITemplateMethod;
+import com.digisphere.propertize.application.director.TemplateMethodPattern.TemplateClass.TemplateMethodDirector;
+import com.digisphere.propertize.application.observerPattern.subject.EventManager;
+import com.digisphere.propertize.application.observerPattern.subject.IEventManager;
+import com.digisphere.propertize.application.user.useCase.CreateUser;
+import com.digisphere.propertize.application.user.useCase.DisableUser;
+import com.digisphere.propertize.application.user.useCase.GetOneUser;
+import com.digisphere.propertize.application.user.useCase.UpdateUser;
+import com.digisphere.propertize.application.user.useCase.interfaces.ICreateUser;
+import com.digisphere.propertize.application.user.useCase.interfaces.IDisableUser;
+import com.digisphere.propertize.application.user.useCase.interfaces.IGetOneUSer;
+import com.digisphere.propertize.application.user.useCase.interfaces.IUpdateUser;
+import com.digisphere.propertize.infra.repository.stateContext.IRepositoryContext;
+import com.digisphere.propertize.infra.repository.stateContext.RepositoryContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserAdapterTest {
+
+    private ICreateUser createUser;
+    private IGetOneUSer getOneUSer;
+    private IDisableUser disableUser;
+    private IUpdateUser updateUser;
+    private IRepositoryContext repositoryContext;
+    private IEventManager eventManager;
+    private ITemplateMethod templateMethod;
+    private IAdapterUser adapterUser;
+
+    @BeforeEach
+    public void setup() {
+        repositoryContext = new RepositoryContext();
+        eventManager = new EventManager();
+        templateMethod = new TemplateMethodDirector();
+        createUser = new CreateUser(repositoryContext, eventManager, templateMethod);
+        getOneUSer = new GetOneUser(repositoryContext);
+        updateUser = new UpdateUser(repositoryContext);
+        disableUser = new DisableUser(repositoryContext);
+
+        adapterUser = new UserAdapter(createUser, updateUser, getOneUSer, disableUser);
+    }
+
+    @Test
+    @DisplayName("Deve adaptar a requisicao de criar um usuário")
+    void adapterToCreateUsers() {
+        var dto = new CreateUserDto("Peter Parker", "peter@email.com", "45732543074", "19 8577-3341", "admin");
+        OutputUser user = adapterUser.adaptCreateUserRequest(dto);
+
+       assertThat(user.email()).isEqualTo("peter.parker@propertize.com");
+    }
+
+    @Test
+    @DisplayName("Deve adaptar a requisicao de atualizar senha de um usuário")
+    void adapterToUpdateUserPassword() {
+        var dto = new UserReferenceDto("58158076050", "senhaForte123", null);
+        String response = adapterUser.adaptUpdateUserRequest(dto);
+
+        assertThat(response).isEqualTo("PASSWORD DO USUÁRIO COM CPF: 58158076050 ATUALIZADA COM SUCESSO!");
+    }
+
+    @Test
+    @DisplayName("Deve adaptar a requisicao de atualizar telefone de um usuário")
+    void adapterToUpdateUserPhone() {
+        var dto = new UserReferenceDto("58158076050", "avenger", "25 2222-3333");
+
+        String response = adapterUser.adaptUpdateUserRequest(dto);
+
+        assertThat(response).isEqualTo("PHONE DO USUÁRIO COM CPF: 58158076050 ATUALIZADA COM SUCESSO!");
+    }
+}
