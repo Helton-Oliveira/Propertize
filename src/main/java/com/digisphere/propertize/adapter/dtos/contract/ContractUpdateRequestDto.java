0369f7dab7c5ef96489c6c319db5718b5351d84b
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-11-13 22:41:09 -0300

    refactor: refactoring to include error phrases for invalid input data

--- src/main/java/com/digisphere/propertize/adapter/dtos/contract/ContractUpdateRequestDto.java
+++ src/main/java/com/digisphere/propertize/adapter/dtos/contract/ContractUpdateRequestDto.java
@@ -3,11 +3,11 @@ package com.digisphere.propertize.adapter.dtos.contract;
 import jakarta.validation.constraints.NotBlank;
 
 public record ContractUpdateRequestDto(
-        @NotBlank
+        @NotBlank (message = "ERRO! IMPOSSÍVEL ENCERRAR CONTRATO SEM DENIFIR O IDENTIFICADOR")
         String id,
-        @NotBlank
+        @NotBlank (message = "ERRO! IMPOSSÍVEL ENCERRAR CONTRATO SEM DENIFIR A DATA DE TÉRMINO")
         String terminationDate,
-        @NotBlank
+        @NotBlank (message = "ERRO! IMPOSSÍVEL ENCERRAR CONTRATO SEM DENIFIR A RAZÃO DO ENCERRAMENTO DO CONTRATO")
         String terminationReason
 ) {
 }
--- src/main/java/com/digisphere/propertize/adapter/dtos/contract/CreateContractDto.java
+++ src/main/java/com/digisphere/propertize/adapter/dtos/contract/CreateContractDto.java
@@ -3,15 +3,15 @@ package com.digisphere.propertize.adapter.dtos.contract;
 import jakarta.validation.constraints.NotBlank;
 
 public record CreateContractDto(
-        @NotBlank
+        @NotBlank (message = "ERRO! IMPOSSÍVEL CRIAR CONTRATO SEM DENIFIR O IDENTIFICADOR DA PROPRIEDADE")
         String propertyId,
-        @NotBlank
+        @NotBlank (message = "ERRO! IMPOSSÍVEL CRIAR CONTRATO SEM DENIFIR O IDENTIFICADOR DO INQUILINO")
         String tenantCpf,
-        @NotBlank
+        @NotBlank(message = "ERRO! IMPOSSÍVEL CRIAR CONTRATO SEM DENIFIR O PERIODO")
         String period,
-        @NotBlank
+        @NotBlank (message = "ERRO! IMPOSSÍVEL CRIAR CONTRATO SEM DENIFIR O DIA DO VENCIMENTO DO PAGAMENTO")
         String paymentDueDay,
-        @NotBlank
+        @NotBlank (message = "ERRO! IMPOSSÍVEL CRIAR CONTRATO SEM DENIFIR O DEPOSITO ADIANTADO")
         String securityDeposit
 ) {
 }
